adae908e150ddfe369788335e48e716f43b33dbe
Chris Smith <csmith@platformconsulting.io>
2020-07-13 13:17:57 -0400

    Bind values from HTML to TS

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,8 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  base = '';
+  weapon = '';
+  unit_1 = '';
+  unit_2 = '';
+  unit_3 = '';
+  mainclass_1 = '';
+  mainclass_2 = '';
+  subclass_1 = '';
+  subclass_2 = '';
+  territory = '';
+  bullet = '';
+  skillCost = '';
+
+  output = 0;
   constructor() { }
 
+  getTotalPP(): number {
+
+    return Number(this.base) +
+    Number(this.weapon) +
+    Number(this.unit_1) +
+    Number(this.unit_2) +
+    Number(this.unit_3) +
+      Number(this.mainclass_1) +
+        Number(this.mainclass_2) +
+          Number(this.subclass_1) +
+            Number(this.subclass_2);
+  }
+
+  caclulate(): void {
+    console.log('Hello, you clicked me');
+    console.log(this.getTotalPP());
+    this.output = this.getTotalPP();
+  }
   ngOnInit(): void {
   }
 
